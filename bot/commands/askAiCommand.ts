1f19f3bb97980fe16c5ad0779ba7db5cdeb43689
Mike Huang <wellwind@mail2000.com.tw>
2023-01-01 02:11:30 +0800

    feat: 當來源是 line 時，所有訊息都會處理，所以要指定指令

--- bot/commands/askAiCommand.ts
+++ bot/commands/askAiCommand.ts
@@ -4,9 +4,11 @@ import { addChatHistory, getChatHistory } from "../helpers/dbHelper";
 import { getOpenAiResponse } from "../helpers/openAiHelper";
 import { ICommand } from "./ICommand";
 
-
 export const askAiCommand: ICommand = {
   checkCommand: (message: string, context: TurnContext) => {
+    if (context.activity.channelId === "line") {
+      return message.startsWith("askChatGPT");
+    }
     return true;
   },
   processCommand: async (
@@ -15,8 +17,15 @@ export const askAiCommand: ICommand = {
     fromId: string,
     conversationId: string
   ) => {
+    let askQuestion = "";
+    if (context.activity.channelId === "line") {
+      askQuestion = message.replace("askChatGPT", "").trim();
+    } else {
+      askQuestion = message;
+    }
+
     const apiKey = await checkApiKey(context, fromId);
-    if(!apiKey) {
+    if (!apiKey) {
       return;
     }
 
@@ -24,19 +33,18 @@ export const askAiCommand: ICommand = {
     const chatHistories = await getChatHistory(fromId, conversationId);
 
     // push human says
-    chatHistories.push(`Human: ${message}`);
+    chatHistories.push(`Human: ${askQuestion}`);
     // create prompt
-    var instruction = '以下是和 AI 助理的對話，AI 助理提供友善且詳盡的回答，使用 markdown 語法回覆，如果需要範例程式，會盡可能提供完整的程式碼。\n\n';
-    const prompt = (chatHistories.join("\n") + "\nAI:").slice(-1024 + instruction.length);
+    var instruction =
+      "以下是和 AI 助理的對話，AI 助理提供友善且詳盡的回答，使用 markdown 語法回覆，如果需要範例程式，會盡可能提供完整的程式碼。\n\n";
+    const prompt = (chatHistories.join("\n") + "\nAI:")
+      .slice(-1024 + instruction.length);
 
     // request success
-    var response = await getOpenAiResponse(
-      apiKey, 
-      `${instruction}${prompt}`, 
-      {
-        presence_penalty: 0.6,
-        stop: ["Human:", "AI:"]
-      });
+    var response = await getOpenAiResponse(apiKey, `${instruction}${prompt}`, {
+      presence_penalty: 0.6,
+      stop: ["Human:", "AI:"],
+    });
 
     // add human says to history
     await addChatHistory(fromId, conversationId, `Human: ${message}`);
--- bot/commands/helpCommand.ts
+++ bot/commands/helpCommand.ts
@@ -11,14 +11,20 @@ export const helpCommand: ICommand = {
     fromId: string,
     conversationId: string
   ) => {
+    let helpText = `* \`setApiKey <API_KEY>\`: 設定 API key；**注意**，不要在別人看得到的頻道設定 API key
+      \n* \`deleteApiKey\`: 刪除 API key
+      \n* \`forgetConversation\`: 忘記過去的對話記錄
+      \n* \`writeCode <指令>\`: 輸入指令，叫 AI 寫程式`;
+    if (context.activity.channelId === "line") {
+      helpText += `\n* \`askChatGPT <問題>\`: 輸入問題，叫 AI 回答`;
+    } else {
+      helpText += `\n* 直接輸入文字，就可以跟 AI 對話了`;
+    }
+
     await context.sendActivity({
       type: "message",
       textFormat: "markdown",
-      text: `* \`setApiKey <API_KEY>\`: 設定 API key；**注意**，不要在別人看得到的頻道設定 API key
-              \n* \`deleteApiKey\`: 刪除 API key
-              \n* \`forgetConversation\`: 忘記過去的對話記錄
-              \n* \`writeCode <指令>\`: 輸入指令，叫 AI 寫程式
-              \n* 直接輸入文字，就可以跟 AI 對話了`,
+      text: helpText,
     });
   },
 };
